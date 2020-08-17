8ccacba93da7277eed6e7146ab72f7a566bcc680
dado200 <66723413+dado200@users.noreply.github.com>
2020-08-17 16:15:07 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzQwMjU0NzY0NjA3OTk1OTQ0.XymV-Q.WkoZUyxa5-Ab_7-xtJqaiM8wjZI", //تعديل مهم توكن بوتك
+  YT_API_KEY: "api ",
+  prefix: "=",
+devs: ["490521551427600394"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+  };
